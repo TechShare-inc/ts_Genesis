931ab97f840b6017d591191b847d92fa951ae1c5
YutoFukuda1113 <yuto.fukuda@techshare.co.jp>
2025-07-28 12:00:13 +0900

    add mirror

---
+++ genesis/utils/mirror.py
@@ -0,0 +1,47 @@
+def mirror_linear(linear):
+    linear_mirrored = linear.clone()
+    linear_mirrored[..., 1] *= -1 # y
+    return linear_mirrored
+
+def mirror_angle(angle):
+    angle_mirrored = angle.clone()
+    angle_mirrored[..., 0] *= -1 # roll
+    angle_mirrored[..., 2] *= -1 # yaw
+    return angle_mirrored
+
+def mirror_commands(commands):
+    commands_mirrored = commands.clone()
+    commands_mirrored[..., 1] *= -1 # y
+    commands_mirrored[..., 2] *= -1 # yaw
+    return commands_mirrored
+
+def mirror_go_joint(joint):
+    joint_mirrored = joint.clone()
+    joint_mirrored[..., [0, 1, 2]] = joint[..., [3, 4, 5]]
+    joint_mirrored[..., [3, 4, 5]] = joint[..., [0, 1, 2]]
+    joint_mirrored[..., [6, 7, 8]] = joint[..., [9, 10, 11]]
+    joint_mirrored[..., [9, 10, 11]] = joint[..., [6, 7, 8]]
+    joint_mirrored[..., 0::3] = -joint_mirrored[..., 0::3] # hip joint
+    return joint_mirrored
+
+def mirror_go_foot(foot):
+    foot_mirrored = foot.clone()
+    foot_mirrored[..., 0] = foot[..., 1]
+    foot_mirrored[..., 1] = foot[..., 0]
+    foot_mirrored[..., 2] = foot[..., 3]
+    foot_mirrored[..., 3] = foot[..., 2]
+    return foot_mirrored
+
+def mirror_hg_joint(joint):
+    joint_mirrored = joint.clone()
+    joint_mirrored[..., [0, 1, 2, 3, 4, 5]] = joint[..., [6, 7, 8, 9, 10, 11]]
+    joint_mirrored[..., [6, 7, 8, 9, 10, 11]] = joint[..., [0, 1, 2, 3, 4, 5]]
+    joint_mirrored[..., [1, 2, 5]] = -joint_mirrored[..., [1, 2, 5]] # leg roll, yaw joint
+    joint_mirrored[..., [7, 8, 11]] = -joint_mirrored[..., [7, 8, 11]] # leg roll, yaw joint
+    return joint_mirrored
+
+def mirror_hg_foot(foot):
+    foot_mirrored = foot.clone()
+    foot_mirrored[..., 0] = foot[..., 1]
+    foot_mirrored[..., 1] = foot[..., 0]
+    return foot_mirrored
