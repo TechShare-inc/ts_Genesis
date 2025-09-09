a4c943a377eaa3312d6dda14be4909b14e56c12f
YutoFukuda1113 <yuto.fukuda@techshare.co.jp>
2025-09-09 10:58:33 +0900

    begin teacher-student

---
+++ rsl_rl-2.2.4/rsl_rl/modules/teacher_student copy.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import torch
+import torch.nn as nn
+from torch.distributions import Normal
+
+from rsl_rl.utils import resolve_nn_activation, unpad_trajectories
+
+class TeacherStudent(nn.Module):
+    is_recurrent = False
+
+    def __init__(
+        self,
+        num_obs,
+        num_privileged_obs,
+        num_teacher_obs,
+        student_history_length,
+        num_actions,
+        actor_hidden_dims=[256, 256, 256],
+        critic_hidden_dims=[256, 256, 256],
+        latent_dims = 256,
+        student_dims = [],
+        teacher_dims = [],
+        activation="elu",
+        init_noise_std=1.0,
+        noise_std_type: str = "scalar",
+        **kwargs,
+    ):
+        if kwargs:
+            print(
+                "TeacherStudent.__init__ got unexpected arguments, which will be ignored: "
+                + str([key for key in kwargs.keys()])
+            )
+        super().__init__()
+        activation = resolve_nn_activation(activation)
+
+        mlp_input_dim_a = num_obs + latent_dims
+        mlp_input_dim_c = num_privileged_obs + latent_dims
+
+        # Policy
+        actor_layers = []
+        actor_layers.append(nn.Linear(mlp_input_dim_a, actor_hidden_dims[0]))
+        actor_layers.append(activation)
+
+        for layer_index in range(len(actor_hidden_dims)):
+            if layer_index == len(actor_hidden_dims) - 1:
+                actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], num_actions))
+            else:
+                actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], actor_hidden_dims[layer_index + 1]))
+                actor_layers.append(activation)
+        self.actor = nn.Sequential(*actor_layers)
+
+        # Value function
+        critic_layers = []
+        critic_layers.append(nn.Linear(mlp_input_dim_c, critic_hidden_dims[0]))
+        critic_layers.append(activation)
+        for layer_index in range(len(critic_hidden_dims)):
+            if layer_index == len(critic_hidden_dims) - 1:
+                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], 1))
+            else:
+                critic_layers.append(nn.Linear(critic_hidden_dims[layer_index], critic_hidden_dims[layer_index + 1]))
+                critic_layers.append(activation)
+        self.critic = nn.Sequential(*critic_layers)
+
+        # Proprioceptive Encoder (Student)
+        self.student = nn.LSTM(input_size=num_obs)
